c95b8cd9dafaa19abf077b5e2f39d30b56358aef
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-04 16:54:14 -0400

    feat: dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { workoutReducer } from './reducers/workout.reducer';
 
@@ -17,6 +18,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     StoreModule.forRoot({ 
       workout: workoutReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10 // number of states to retain
     }),
     FormsModule
   ],
